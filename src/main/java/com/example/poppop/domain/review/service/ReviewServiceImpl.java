7e6597a2ecd3d46c3fc74bd9bb9721ee961f6610
JonghyeokNam <njh9750@naver.com>
2025-07-01 13:43:14 +0900

    Refactor(#25): 리뷰 전체 조회 시 댓글 개수, 좋아요 개수 반환 추가

--- src/main/java/com/example/poppop/domain/review/service/ReviewServiceImpl.java
+++ src/main/java/com/example/poppop/domain/review/service/ReviewServiceImpl.java
@@ -66,12 +66,20 @@ public class ReviewServiceImpl implements ReviewService {
         Popup popup = popupRepository.findById(popupId)
                 .orElseThrow(() -> new CustomException(ReviewErrorCode.POPUP_NOT_FOUND));
 
-        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+        Pageable pageable = PageRequest.of(page - 1, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+
+
 
         return reviewRepository
                 .findByPopupAndIsDeletedFalseOrderByCreatedAtDesc(popup, pageable)
                 .stream()
-                .map(ReviewResponse::from)
+                .map(review -> {
+                    long likeCnt = reviewLikeRepository
+                            .countByReviewAndLikedTrue(review);
+                    long commentCnt = commentRepository
+                            .countByReviewAndParentIsNullAndIsDeletedFalse(review);
+                    return ReviewResponse.from(review, likeCnt, commentCnt);
+                })
                 .toList();
     }
 
