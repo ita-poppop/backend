e137ed43832dec017c0b2e6ebe32412eb0ca3583
JonghyeokNam <njh9750@naver.com>
2025-07-08 00:56:00 +0900

    Refactor(#23): 알림 관련 코드 주석 처리

--- src/main/java/com/example/poppop/domain/notification/repository/DeviceTokenRepository.java
+++ src/main/java/com/example/poppop/domain/notification/repository/DeviceTokenRepository.java
@@ -1,14 +1,14 @@
-package com.example.poppop.domain.notification.repository;
-
-import com.example.poppop.domain.member.entity.Member;
-import com.example.poppop.domain.notification.entity.DeviceToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface DeviceTokenRepository extends JpaRepository<DeviceToken, Long> {
-
-    List<DeviceToken> findAllByMember(Member member);
-    boolean existsByMemberAndToken(Member member, String token);
-    void deleteByMemberAndToken(Member member, String token);
-}
+//package com.example.poppop.domain.notification.repository;
+//
+//import com.example.poppop.domain.member.entity.Member;
+//import com.example.poppop.domain.notification.entity.DeviceToken;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface DeviceTokenRepository extends JpaRepository<DeviceToken, Long> {
+//
+//    List<DeviceToken> findAllByMember(Member member);
+//    boolean existsByMemberAndToken(Member member, String token);
+//    void deleteByMemberAndToken(Member member, String token);
+//}
