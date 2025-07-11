c8ec82df2ade7fe932edca374bcb3af2d3cfb621
JonghyeokNam <njh9750@naver.com>
2025-07-12 03:28:06 +0900

    FirebaseConfig 수정

--- src/main/java/com/example/poppop/global/config/FirebaseConfig.java
+++ src/main/java/com/example/poppop/global/config/FirebaseConfig.java
@@ -1,6 +1,7 @@
 package com.example.poppop.global.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -17,21 +18,36 @@ public class FirebaseConfig {
     @Value("${firebase.service-account-file}")  // application.yml 에 경로
     private String serviceAccountPath;
 
-    @Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream(serviceAccountPath);
-
+//    @Bean
+//    public FirebaseApp firebaseApp() throws IOException {
+//        FileInputStream serviceAccount = new FileInputStream(serviceAccountPath);
+//
+////        FirebaseOptions options = FirebaseOptions.builder()
+////                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+////                .build();
+////
+////        return FirebaseApp.initializeApp(options);
+//        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+//
 //        FirebaseOptions options = FirebaseOptions.builder()
-//                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                .setCredentials(credentials)
+//                // JSON의 project_id 값을 그대로 넣어 줍니다
+//                .setProjectId("poppop-6d345")
 //                .build();
 //
 //        return FirebaseApp.initializeApp(options);
-        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+//    }
 
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        // 서비스 계정 JSON 파일을 ServiceAccountCredentials 로 로드
+        FileInputStream serviceAccount = new FileInputStream(serviceAccountPath);
+        ServiceAccountCredentials credentials = ServiceAccountCredentials.fromStream(serviceAccount);
+
+        // JSON 안의 project_id 를 꺼내서 setProjectId() 에 넣어 줍니다
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(credentials)
-                // JSON의 project_id 값을 그대로 넣어 줍니다
-                .setProjectId("poppop-6d345")
+                .setProjectId(credentials.getProjectId())
                 .build();
 
         return FirebaseApp.initializeApp(options);
