cf45c07ebded5a058b4a175e095b89b3bbb80e96
JonghyeokNam <njh9750@naver.com>
2025-07-11 19:18:38 +0900

    리뷰 좋아요 알림 추가

--- src/main/java/com/example/poppop/domain/review/service/ReviewLikeServiceImpl.java
+++ src/main/java/com/example/poppop/domain/review/service/ReviewLikeServiceImpl.java
@@ -7,11 +7,13 @@ import com.example.poppop.domain.review.dto.response.ReviewLikeResponse;
 import com.example.poppop.domain.review.entity.Review;
 import com.example.poppop.domain.review.entity.ReviewLike;
 import com.example.poppop.domain.review.error.ReviewLikeErrorCode;
+import com.example.poppop.domain.review.event.ReviewLikedEvent;
 import com.example.poppop.domain.review.repository.ReviewLikeRepository;
 import com.example.poppop.domain.review.repository.ReviewRepository;
 import com.example.poppop.global.auth.model.PopPopOAuth2User;
 import com.example.poppop.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +25,7 @@ public class ReviewLikeServiceImpl implements ReviewLikeService {
     private final ReviewLikeRepository reviewLikeRepository;
     private final ReviewRepository reviewRepository;
     private final MemberRepository memberRepository;
+    private final ApplicationEventPublisher publisher;
 
     @Override
     public ReviewLikeResponse toggle(Long reviewId, PopPopOAuth2User oauth2User) {
@@ -38,6 +41,11 @@ public class ReviewLikeServiceImpl implements ReviewLikeService {
 
         like.toggle();
         long cnt = reviewLikeRepository.countByReviewAndLikedTrue(review);
+
+        if (like.getLiked()
+                && !review.getMember().getId().equals(member.getId())) {
+            publisher.publishEvent(new ReviewLikedEvent(this, like));
+        }
 
         return new ReviewLikeResponse(like.getLiked(), cnt);
     }
